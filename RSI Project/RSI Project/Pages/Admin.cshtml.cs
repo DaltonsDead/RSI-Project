dd16b9af7d2832f4914e8cba80ec4ca45bbc611e
Emma Dusseau <edusseau@smartweb.augustatech.edu>
2023-03-08 17:47:50 -0500

    Added Training Plan functions UI

---
+++ RSI Project/RSI Project/Pages/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RSI_Project.Pages
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
