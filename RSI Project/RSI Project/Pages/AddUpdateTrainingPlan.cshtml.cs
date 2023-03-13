1aed936e63d879c0941a720365936dde58932e1d
Emma Dusseau <edusseau@smartweb.augustatech.edu>
2023-03-13 14:40:06 -0400

    Added TrainingHistory, Admin, ActiveTraining pieces & skills section to Personal

---
+++ RSI Project/RSI Project/Pages/AddUpdateTrainingPlan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RSI_Project.Pages
+{
+    public class AddUpdateTrainingPlanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
